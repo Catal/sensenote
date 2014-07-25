b3fde8eb77babcc98200b426cdcaee40a69ac434
Takuya Uehara <indigo.lain@gmail.com>
2014-07-25 21:24:15 +0900

    added users_groups model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140725115220) do
+ActiveRecord::Schema.define(version: 20140725121742) do
 
   create_table "admin_users", force: true do |t|
     t.string   "email",               default: "", null: false
@@ -59,5 +59,12 @@ ActiveRecord::Schema.define(version: 20140725115220) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "users_groups", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
