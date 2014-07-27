5ba92f8c7ece0121eb6cd6a5432110dd2fb46205
Takuya Uehara <indigo.lain@gmail.com>
2014-07-27 22:36:45 +0900

    added group model and relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :users_groups
+  has_many :groups, :through => :users_groups
 end
--- app/models/users_groups.rb
+++
@@ -1,2 +0,0 @@
-class UsersGroups < ActiveRecord::Base
-end
