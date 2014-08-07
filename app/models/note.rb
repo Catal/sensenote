0639ad830a791c3cf911e898b1f75fb82588fc43
tmutocatal <tmutopop@gmail.com>
2014-08-07 16:13:52 +0900

    added search function for title and body

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,11 @@
 class Note < ActiveRecord::Base
+
+  def self.search(search)
+    if search.blank?
+      Note.all
+    else
+      Note.where(['title LIKE ? OR body LIKE ?', "%#{search}%"])
+    end
+  end
+
 end
