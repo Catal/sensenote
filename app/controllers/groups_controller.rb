0360806b45dc9918235c9d96f50a21d298d1db61
Takuya Uehara <indigo.lain@gmail.com>
2014-08-07 15:14:10 +0900

    group and user relation updated

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -28,7 +28,6 @@ class GroupsController < ApplicationController
   # POST /groups.json
   def create
     @group = Group.new(group_params)
-
     respond_to do |format|
       if @group.save
         format.html { redirect_to @group, notice: 'Group was successfully created.' }
@@ -72,6 +71,6 @@ class GroupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
-      params.require(:group).permit(:name)
+      params.require(:group).permit(:name,user_ids: [])
     end
 end
