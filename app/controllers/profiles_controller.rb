49c6c6b4b488af39db3921c9cc5f8dc431971d6b
Takuya Uehara <indigo.lain@gmail.com>
2014-08-08 19:56:57 +0900

    added avatar uploader

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -69,6 +69,6 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params[:profile]
+      params[:profile].permit(:name, :avatar)
     end
 end
