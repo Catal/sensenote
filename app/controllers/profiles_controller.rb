ae422bae0f9f6bd37fbe4d3755c0340b7f5dd33b
Takuya Uehara <indigo.lain@gmail.com>
2014-08-14 13:03:16 +0900

    profile show updated

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,6 +10,8 @@ class ProfilesController < ApplicationController
   # GET /profiles/1
   # GET /profiles/1.json
   def show
+    @profile_id = Profile.find(params[:id]).id
+    @groups = User.find(@profile_id).groups
   end
 
   # GET /profiles/new
