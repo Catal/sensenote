8ecccc16ac4096c86c879eb36ffbff7b9e031fb6
Takuya Uehara <indigo.lain@gmail.com>
2014-09-11 17:01:12 +0900

    modified config for ckeditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,11 +1,17 @@
-CKEDITOR.config.toolbarGroups = [
-// { name: 'links' },
-                { name: 'insert' },
-                    { name: 'forms' },
-                        { name: 'tools' },
-                                { name: 'others' },
-                                    '/',
-                                        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-                                                { name: 'styles' },
-                                                    { name: 'colors' },
-                                                        ];
+CKEDITOR.config.toolbar = [
+  { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+  { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+  { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+  { name: 'insert', items: ['Image', 'Table', 'HorizontalRule'] },
+  { name: 'forms', items: [  'Checkbox' ] },
+  '/',
+  { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote',  '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-' ] },
+  { name: 'tools', items: [ 'Maximize' ] },
+  { name: 'forms' },
+  { name: 'tools' },
+  { name: 'others' },
+  '/',
+  { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+  { name: 'styles' },
+  { name: 'colors' },
+];
