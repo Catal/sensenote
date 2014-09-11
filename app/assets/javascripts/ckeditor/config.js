6d9b2de789944acedad22749d510d701b3f0f992
Takuya Uehara <indigo.lain@gmail.com>
2014-09-11 16:09:36 +0900

    added config for ckeditor

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,15 @@
+
+CKEDITOR.config.toolbar = [
+    { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+        { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+            { name: 'links' },
+                { name: 'insert' },
+                    { name: 'forms' },
+                        { name: 'tools' },
+                            { name: 'document',    groups: [ 'mode', 'document', 'doctools' ] },
+                                { name: 'others' },
+                                    '/',
+                                        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+                                                { name: 'styles' },
+                                                    { name: 'colors' },
+                                                        ];
