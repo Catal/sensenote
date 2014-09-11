9b2e1e0d6a119224ac2783e567f0341850d39da2
tmutocatal <tmutopop@gmail.com>
2014-09-11 18:57:11 +0900

    updated japanese

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,7 @@ class Note < ActiveRecord::Base
   has_many :notes_groups
   has_many :groups, :through => :notes_groups
 
+  validates_presence_of :title, :body
   def self.search(search)
     if search.blank?
       Note.all
