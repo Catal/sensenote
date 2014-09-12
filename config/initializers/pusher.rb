8e30943c412abf6b7f8eaa9f376875f391ff3d4d
tmutocatal <tmutopop@gmail.com>
2014-09-12 16:06:39 +0900

    utf8

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -3,6 +3,5 @@ require 'pusher'
 Pusher.app_id = '89093'
 Pusher.key = 'be77e6f1a3890641c69f'
 Pusher.secret = '3b77221798b0474b21c4'
-
 Pusher.url = "http://be77e6f1a3890641c69f:3b77221798b0474b21c4@api.pusherapp.com/apps/89093"
 Pusher.logger = Rails.logger
