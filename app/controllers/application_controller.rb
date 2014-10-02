ed88495103121459a1dfee343a8e31357dbf0999
Takuya Uehara <indigo.lain@gmail.com>
2014-10-02 18:06:00 +0900

    changed group page layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   helper_method :current_user_avatar
+  before_filter :store_location
 
 
   def current_user_avatar
     if current_user.profile.blank? == false
       @current_user_avatar = current_user.profile
     end
+  end
+
+  def store_location
+    if session[:request_from]
+      @prev_page = session[:request_from]
+    end
+    session[:request_from] = request.fullpath
   end
 end
